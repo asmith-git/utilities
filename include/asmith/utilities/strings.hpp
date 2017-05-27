1fa4fff71bd6ade349b9838e5c67eb0f204ffbc4
Adam Smith <adam.george.smith@btinternet.com>
2017-05-27 12:08:07 +0100

    String function definitions

---
+++ include/asmith/utilities/strings.hpp
@@ -0,0 +1,50 @@
+//	Copyright 2017 Adam Smith
+//	Licensed under the Apache License, Version 2.0 (the "License") throw();
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+// 
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+#ifndef ASMITH_UTILITIES_STRINGS_HPP
+#define ASMITH_UTILITIES_STRINGS_HPP
+
+namespace asmith {
+	char to_upper_case(char) throw();
+	void to_upper_case(char*) throw();
+	void to_upper_case(char*, size_t) throw();
+
+	char to_lower_case(char) throw();
+	void to_lower_case(char*) throw();
+	void to_lower_case(char*, size_t) throw();
+
+	bool is_upper_case(char) throw();
+	bool is_upper_case(const char*) throw();
+	bool is_upper_case(const char*, size_t) throw();
+
+	bool is_lower_case(char) throw();
+	bool is_lower_case(const char*) throw();
+	bool is_lower_case(const char*, size_t) throw();
+
+	bool is_letter(char) throw();
+	bool is_letter(const char*) throw();
+	bool is_letter(const char*, size_t) throw();
+
+	bool is_vowel(char) throw();
+	bool is_vowel(const char*) throw();
+	bool is_vowel(const char*, size_t) throw();
+
+	bool is_consonant(char) throw();
+	bool is_consonant(const char*) throw();
+	bool is_consonant(const char*, size_t) throw();
+
+	bool is_number(char) throw();
+	bool is_number(const char*) throw();
+	bool is_number(const char*, size_t) throw();
+}
+#endif
---
+++ src/asmith/utilities/strings.cpp
@@ -0,0 +1,133 @@
+//	Copyright 2017 Adam Smith
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+// 
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+#include "asmith/utilities/strings.hpp"
+
+namespace asmith {
+	char to_upper_case(char aChar) throw() {
+		//! \todo Implement
+		return '\0';
+	}
+
+	void to_upper_case(char* aStr) throw() {
+		//! \todo Implement
+	}
+
+	void to_upper_case(char* aStr, size_t aSize) throw() {
+		//! \todo Implement
+	}
+
+	char to_lower_case(char aChar) throw() {
+		//! \todo Implement
+		return '\0';
+	}
+
+	void to_lower_case(char* aStr) throw() {
+		//! \todo Implement
+	}
+
+	void to_lower_case(char* aStr, size_t aSize) throw() {
+		//! \todo Implement
+	}
+
+	bool is_upper_case(char aChar) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_upper_case(const char* aStr) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_upper_case(const char* aStr, size_t aSize) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_lower_case(char aChar) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_lower_case(const char* aStr) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_lower_case(const char* aStr, size_t aSize) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_letter(char aChar) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_letter(const char* aStr) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_letter(const char* aStr, size_t aSize) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_vowel(char aChar) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_vowel(const char* aStr) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_vowel(const char* aStr, size_t aSize) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_consonant(char aChar) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_consonant(const char* aStr) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_consonant(const char* aStr, size_t aSize) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_number(char aChar) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_number(const char* aStr) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+	bool is_number(const char* aStr, size_t aSize) throw() {
+		//! \todo Implement
+		return false;
+	}
+
+}
