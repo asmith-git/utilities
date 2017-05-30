a3b046ac38935cab79dd7c3294b511357f3fc980
Adam Smith <adam.george.smith@btinternet.com>
2017-05-30 10:11:38 +0100

    Header for reflect functions

---
+++ src/asmith/utilities/reflect.cpp
@@ -0,0 +1,57 @@
+//	Copyright 2017 Adam Smith
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+// 
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+#include "asmith/utilities/reflect.hpp"
+
+namespace asmith {
+	uint8_t reflect(uint8_t aValue) throw() {
+		//! \todo Implement
+		return 0;
+	}
+
+	uint16_t reflect(uint16_t aValue) throw() {
+		//! \todo Implement
+		return 0;
+	}
+
+	uint32_t reflect(uint32_t aValue) throw() {
+		//! \todo Implement
+		return 0;
+	}
+
+	uint64_t reflect(uint64_t aValue) throw() {
+		//! \todo Implement
+		return 0;
+	}
+
+	int8_t reflect(int8_t aValue) throw() {
+		//! \todo Implement
+		return 0;
+	}
+
+	int16_t reflect(int16_t aValue) throw() {
+		//! \todo Implement
+		return 0;
+	}
+
+	int32_t reflect(int32_t aValue) throw() {
+		//! \todo Implement
+		return 0;
+	}
+
+	int64_t reflect(int64_t aValue) throw() {
+		//! \todo Implement
+		return 0;
+	}
+
+}
