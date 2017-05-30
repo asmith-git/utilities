2774f22ca102ed16bdd84f14c54a322979230e96
Adam Smith <adam.george.smith@btinternet.com>
2017-05-30 10:19:22 +0100

    Defered implementation to reflect(uint8_t)

--- src/asmith/utilities/reflect.cpp
+++ src/asmith/utilities/reflect.cpp
@@ -14,44 +14,92 @@
 #include "asmith/utilities/reflect.hpp"
 
 namespace asmith {
+
+	static constexpr uint8_t REFLECTION_LOOKUP[16] = {
+		0b0001
+	};
+
 	uint8_t reflect(uint8_t aValue) throw() {
 		//! \todo Implement
 		return 0;
 	}
 
 	uint16_t reflect(uint16_t aValue) throw() {
-		//! \todo Implement
-		return 0;
+		uint16_t tmp;
+		uint8_t* const dst = reinterpret_cast<uint8_t*>(&tmp);
+		const uint8_t* const src = reinterpret_cast<const uint8_t*>(&aValue);
+		dst[0] = reflect(src[1]);
+		dst[1] = reflect(src[0]);
+		return tmp;
 	}
 
 	uint32_t reflect(uint32_t aValue) throw() {
-		//! \todo Implement
-		return 0;
+		uint32_t tmp;
+		uint8_t* const dst = reinterpret_cast<uint8_t*>(&tmp);
+		const uint8_t* const src = reinterpret_cast<const uint8_t*>(&aValue);
+		dst[0] = reflect(src[3]);
+		dst[1] = reflect(src[2]);
+		dst[2] = reflect(src[1]);
+		dst[3] = reflect(src[0]);
+		return tmp;
 	}
 
 	uint64_t reflect(uint64_t aValue) throw() {
-		//! \todo Implement
-		return 0;
+		uint64_t tmp;
+		uint8_t* const dst = reinterpret_cast<uint8_t*>(&tmp);
+		const uint8_t* const src = reinterpret_cast<const uint8_t*>(&aValue);
+		dst[0] = reflect(src[7]);
+		dst[1] = reflect(src[6]);
+		dst[2] = reflect(src[5]);
+		dst[3] = reflect(src[4]);
+		dst[4] = reflect(src[3]);
+		dst[5] = reflect(src[2]);
+		dst[6] = reflect(src[1]);
+		dst[7] = reflect(src[0]);
+		return tmp;
 	}
 
 	int8_t reflect(int8_t aValue) throw() {
-		//! \todo Implement
-		return 0;
+		int8_t tmp;
+		uint8_t* const dst = reinterpret_cast<uint8_t*>(&tmp);
+		const uint8_t* const src = reinterpret_cast<const uint8_t*>(&aValue);
+		dst[0] = reflect(src[0]);
+		return tmp;
 	}
 
 	int16_t reflect(int16_t aValue) throw() {
-		//! \todo Implement
-		return 0;
+		int16_t tmp;
+		uint8_t* const dst = reinterpret_cast<uint8_t*>(&tmp);
+		const uint8_t* const src = reinterpret_cast<const uint8_t*>(&aValue);
+		dst[0] = reflect(src[1]);
+		dst[1] = reflect(src[0]);
+		return tmp;
 	}
 
 	int32_t reflect(int32_t aValue) throw() {
-		//! \todo Implement
-		return 0;
+		int32_t tmp;
+		uint8_t* const dst = reinterpret_cast<uint8_t*>(&tmp);
+		const uint8_t* const src = reinterpret_cast<const uint8_t*>(&aValue);
+		dst[0] = reflect(src[3]);
+		dst[1] = reflect(src[2]);
+		dst[2] = reflect(src[1]);
+		dst[3] = reflect(src[0]);
+		return tmp;
 	}
 
 	int64_t reflect(int64_t aValue) throw() {
-		//! \todo Implement
-		return 0;
+		int64_t tmp;
+		uint8_t* const dst = reinterpret_cast<uint8_t*>(&tmp);
+		const uint8_t* const src = reinterpret_cast<const uint8_t*>(&aValue);
+		dst[0] = reflect(src[7]);
+		dst[1] = reflect(src[6]);
+		dst[2] = reflect(src[5]);
+		dst[3] = reflect(src[4]);
+		dst[4] = reflect(src[3]);
+		dst[5] = reflect(src[2]);
+		dst[6] = reflect(src[1]);
+		dst[7] = reflect(src[0]);
+		return tmp;
 	}
 
 }
