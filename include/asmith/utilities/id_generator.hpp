a6602518b990fb01207c7f2b8f6c8a4b0f6f00f9
Adam Smith <adam.george.smith@btinternet.com>
2017-06-16 10:09:50 +0100

    Example ID generator

--- include/asmith/utilities/id_generator.hpp
+++ include/asmith/utilities/id_generator.hpp
@@ -14,6 +14,9 @@
 #ifndef ASMITH_UTILITIES_ID_GENERATOR_HPP
 #define ASMITH_UTILITIES_ID_GENERATOR_HPP
 
+#include <cstdint>
+#include <vector>
+
 namespace asmith {
 	
 	/*!
@@ -26,12 +29,79 @@ namespace asmith {
 	template<class T>
 	class id_generator {
 	public:
+		typedef T id_t;
+
 		virtual ~id_generator() {}
 
 		virtual T generate() throw() = 0;
 		virtual bool use(T) throw() = 0;
 		virtual bool free(T) throw() = 0;
 		virtual bool is_used(T) const throw() = 0;
+	};
+
+	/*!
+		\brief A implementation of id_generator for generating and maintaining ID numbers.
+		\param T The type of ID to manage
+		\param R True if ID codes can be reused after they have been freed
+		\version 0.0
+		\data Created : 16th June 2017 Modified : 16th June 2017
+		\author Adam Smith
+	*/
+	template<class T, bool R>
+	class id_generator_example : public id_generator<T> {
+	private:
+		std::vector<T> mUsed;
+		std::vector<T> mFree;
+		uint64_t mBase;
+	public:
+		id_generator_example() :
+			mBase(0)
+		{}
+
+		// Inherited from id_generator
+
+		T generate() throw() override {
+			T id;
+
+			if(R && ! mFree.empty()) {
+				id = mFree.back();
+				mFree.pop_back();
+				return id;
+			}else while(true) {
+				id = static_cast<T>(mBase++);
+				for(const T i : mUsed) if(i == id) continue;
+				return id;
+			}
+			return static_cast<T>(0);
+		}
+
+		bool use(T aID) throw() override {
+			if(is_used(aID)) return false;
+			mUsed.push_back(aID);
+			return true;
+		}
+
+		bool free(T aID) throw() override {
+			const auto end = mUsed.end();
+			for(auto i = mUsed.begin(); i < end; ++i) {
+				if(*i == aID) {
+					mUsed.erase(i);
+					mFree.push_back(aID);
+					return true;
+				}
+			}
+			return false;
+		}
+
+		bool is_used(T aID) const throw() override {
+			if(aID < mBase) {
+				for(const T i : mFree) if(i == aID) return false;
+				return true;
+			}else {
+				for(const T i : mUsed) if(i == aID) return true;
+				return false;
+			}
+		}
 	};
 }
 #endif
