be8c4d968f1358959b11be28e58bad9fc3ceb185
Adam Smith <adam.george.smith@btinternet.com>
2017-06-16 10:23:59 +0100

    Example ID holder

--- include/asmith/utilities/id_holder.hpp
+++ include/asmith/utilities/id_holder.hpp
@@ -14,6 +14,8 @@
 #ifndef ASMITH_UTILITIES_ID_HOLDER_HPP
 #define ASMITH_UTILITIES_ID_HOLDER_HPP
 
+#include "id_generator.hpp"
+
 namespace asmith {
 	
 	/*!
@@ -31,6 +33,42 @@ namespace asmith {
 		virtual ~id_holder() throw() {}
 
 		virtual T get_id() const throw() = 0;
+	};
+
+	/*!
+		\brief 
+		\param T The type of ID to manage
+		\version 0.0
+		\data Created : 16th June 2017 Modified : 16th June 2017
+		\author Adam Smith
+	*/
+	template<class T>
+	class id_holder_example : public_id_holder {
+	private:
+		id_generator<T>& mGenerator;
+		const T mID;
+	public:
+		id_holder_example(id_generator<T>& aGenerator) throw() :
+			mGenerator(aGenerator),
+			mID(aGenerator.generate())
+		{}
+
+		id_holder_example(id_generator<T>& aGenerator, T aID) throw() :
+			mGenerator(aGenerator),
+			mID(aID)
+		{
+			mGenerator.use(aID);
+		}
+
+		virtual ~id_holder_example() throw() {
+			mGenerator.free(mID);
+		}
+
+		// Inherited from id_holder
+
+		T get_id() const throw() override {
+			return mID;
+		}
 	};
 }
 #endif
